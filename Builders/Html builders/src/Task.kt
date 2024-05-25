9fc11e4f5185b58f6228d510326eb3de24e123c8
kadun1 <kadunjj@gmail.com>
2024-05-26 08:45:57 +0900

    Kotlin Koans : html

--- Builders/Html builders/src/Task.kt
+++ Builders/Html builders/src/Task.kt
@@ -1,7 +1,7 @@
 fun renderProductTable(): String {
     return html {
         table {
-            tr/* TODO */ {
+            tr(color = getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -13,7 +13,19 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            TODO()
+            for ((index, product) in products.withIndex()) {
+                tr(color = getCellColor(index, 0)) {
+                    td {
+                        text(product.description)
+                    }
+                    td {
+                        text(product.price)
+                    }
+                    td {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
